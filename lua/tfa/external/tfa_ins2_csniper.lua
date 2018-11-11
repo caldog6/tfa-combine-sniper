a5ce862aa590d9d0ff36a5d82426309fe97cb143
Pyromancer <maddogcommunity@gmail.com>
2018-11-11 15:52:49 +0000

    added the addon
    gun + sound + model = mm yes shoot

---
+++ lua/tfa/external/tfa_ins2_csniper.lua
@@ -0,0 +1,10 @@
+local path = "weapons/tfa_ins2/csniper/"
+
+TFA.AddFireSound("TFA_INS2_CSNIPER.1", path .. "csniper_fire.wav")
+
+TFA.AddWeaponSound("Weapon_csniper.Empty", path .. "csniper_empty.wav")
+
+TFA.AddWeaponSound("Weapon_csniper.Magout", path .. "csniper_magout.wav")
+TFA.AddWeaponSound("Weapon_csniper.Magin", path .. "csniper_magin.wav")
+TFA.AddWeaponSound("Weapon_csniper.Bolt", path .. "csniper_bolting.wav")
+TFA.AddWeaponSound("csniper.Draw", path .. "csniper_draw.wav")
